7958e2834607072f984e8f05f59c21c0d1401716
yasas321 <t2747251@gmail.com>
2025-11-23 06:18:48 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -23,7 +23,7 @@ const {
   jidNormalizedUser,
   downloadContentFromMessage,
   DisconnectReason
-} = require('@whiskeysockets/baileys');
+} = require('baileys');
 
 // ---------------- CONFIG ----------------
 
